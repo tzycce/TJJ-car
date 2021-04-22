0efe5ddcb1fd571605dee2a1eb70bfaa356d7f1b
tzycce <45752945+tzycce@users.noreply.github.com>
2021-04-22 14:08:48 +0000

    Update tjj-car.ino
    Added analog input for the dc-motors so the changes of direction wouldn't be so violent.

--- tjj-car.ino
+++ tjj-car.ino
@@ -1,87 +1,152 @@
-//pins
-const int echoPin = 7;
-const int trigPin = 8;
-int red = 2;
-
 void setup() {
+
+  int red = 2;
+
   // initialize serial communication:
-   Serial.begin(9600);
-   pinMode(red,OUTPUT);
-   pinMode(6,OUTPUT);
-   pinMode(5,OUTPUT);
-   pinMode(4,OUTPUT);
-   pinMode(3,OUTPUT);
+  Serial.begin(9600);
+  pinMode(red, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
 }
 
 void loop() {
-  // establish variables 
+  // establish variables
   long duration, cm;
   // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  const int echoPin = 7;
+  const int trigPin = 8;
+  int red = 2;
+  int speed1;
+  int speed2;
+
   pinMode(trigPin, OUTPUT);
   digitalWrite(trigPin, LOW);
-  
+
   delayMicroseconds(5);
   digitalWrite(trigPin, HIGH);
-  
+
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
+
   pinMode(echoPin, INPUT);
   duration = pulseIn(echoPin, HIGH);
 
   // convert the time into a distance
-  cm = (duration/2) /29.1;
-  
-     //distance less than 15cm 
-  if(cm < 15){
-  
-	// turn on the light
-	digitalWrite(red,HIGH);
-	
-	// reverse direction
-	//motor 1 
-  	digitalWrite(6, LOW);
-  	digitalWrite(5, HIGH);
-  	//motor 2
-  	digitalWrite(4, LOW);
-  	digitalWrite(3, HIGH);
-	}
-	
-    	//distance greater than 30cm
-     if(cm>30){
-	// turn of the light
-	digitalWrite(red,LOW);
-	      
-        //turn the motors normally    
-        // motor 1 
-  	digitalWrite(6, HIGH);
-  	digitalWrite(5, LOW);
-  	//motor 2
-  	digitalWrite(4, HIGH);
-  	digitalWrite(3, LOW);
-      
-	}
-	
-	//distance between 15 and 30 cm
-    if(cm>15 && cm<30){
-  	
-  	//turn off the light
-	digitalWrite(red,LOW);
-	
-	// only turn motor 2 to rotate   
-    	//motor 1 
-  	digitalWrite(6, LOW);
-  	digitalWrite(5, LOW);
-   	//motor 2
-  	digitalWrite(4, HIGH);
-  	digitalWrite(3, LOW);
-          
-	}
+  cm = (duration / 2) / 29.1;
 
-    //print distance to serial		
-    Serial.print(cm);
-    Serial.print("cm");
-    Serial.println();
-   
+  if (cm < 15) {
+    digitalWrite(red, HIGH);
+
+    if (speed1 != -50) {
+      //less than 15 cm
+      //motor 1
+
+
+      for (int i = 128; i >= 0; i--) {
+        analogWrite(10, i);
+        delay(10);
+      }
+
+      digitalWrite(6, LOW);
+      digitalWrite(5, HIGH);
+      digitalWrite(4, LOW);
+      digitalWrite(3, HIGH);
+      for (int i = 0; i < 128; i++) {
+        analogWrite(9, i);
+        analogWrite(10, i);
+        delay(10);
+      }
+      //delay(10);
+      //analogWrite(9, 50);
+      //digitalWrite(6, LOW);
+      //digitalWrite(5, HIGH);
+      speed1 = -50;
+      //motor 2
+      //for (int i=64; i>=0; i--) {
+      //analogWrite(10, i);
+      //delay(10);
+      //}
+      //for (int i=0; i<64; i++) { analogWrite(10, i); delay(10); }
+      //analogWrite(10, 50);
+      //digitalWrite(4, LOW);
+      //digitalWrite(3, HIGH);
+    }
+  }
+
+  if (cm > 30) {
+    // turn of the light
+    digitalWrite(red, LOW);
+    delay(10);
+    if (speed1 != 50) {
+      analogWrite(10, 128);
+
+      analogWrite(9, 128);
+      for (int i = 128; i >= 0; i--) {
+        analogWrite(9, i);
+        analogWrite(10, i);
+        delay(10);
+      }
+      //analogWrite(9, 50);
+      digitalWrite(6, HIGH);
+      digitalWrite(5, LOW);
+      digitalWrite(4, HIGH);
+      digitalWrite(3, LOW);
+      for (int i = 0; i < 255; i++) {
+        analogWrite(9, i);
+        analogWrite(10, i);
+        delay(10);
+      }
+      speed1 = 50;
+      // motor 1
+      //digitalWrite(6, HIGH);
+      //digitalWrite(5, LOW);
+      //motor 2
+      //analogWrite(10, 50);
+      //digitalWrite(4, HIGH);
+      //digitalWrite(3, LOW);
+    }
+  }
+
+  if (cm > 15 && cm < 30) {
+    digitalWrite(red, LOW);
+    if (speed1 != 40) {
+      for (int i = 255; i >= 0; i--) {
+        analogWrite(9, i);
+        analogWrite(10, i);
+        delay(10);
+      }
+      //analogWrite(9, 50);
+      //between 10 and 30 cm
+      //motor 1
+      digitalWrite(6, LOW);
+      digitalWrite(5, LOW);
+      digitalWrite(4, HIGH);
+      digitalWrite(3, LOW);
+      speed1 = 40;
+      for (int i = 0; i < 128; i++) {
+        analogWrite(9, i);
+        analogWrite(10, i);
+        delay(10);
+      }
+      //motor 2
+      //analogWrite(10, 50);
+      //digitalWrite(6, LOW);
+      //digitalWrite(5, LOW);
+      //digitalWrite(4, HIGH);
+      //digitalWrite(3, LOW);
+
+      //  for (int i=0; i<50; i++) { analogWrite(9, i); delay(100); }
+    }
+  }
+
+
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  delay(50);
 }
